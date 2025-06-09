42661d26e35b1b4a039af284775d4e68cd92d8eb
Andreas Schwab <andreas.schwab@softworx.at>
2025-06-09 15:48:56 +0200

    Modifications ...

--- dd_simple_image.sh
+++ dd_simple_image.sh
@@ -6,17 +6,17 @@ source "$CONFIG_FILE"
 
 [[ $EUID -ne 0 ]] && { echo "Run as root"; exit 1; }
 
+[ ! -b "$DISK_DEVICE" ] && { echo "Device not found: $DISK_DEVICE"; exit 1; }
+
 CURRENT_DATE=$(date +"%Y-%m-%d-%H%M")
 BACKUP_FILENAME="image-$CURRENT_DATE.img.xz"
 
-echo "Creating backup (without snapshot)..."
+echo "Creating backup of $DISK_DEVICE..."
 TEMP_FILE="/dev/shm/backup_$$.img.xz"
-LVM_VG_PATH=$(echo "$LVM_VG" | sed 's/-/--/g')
-LVM_LV_PATH=$(echo "$LVM_LV" | sed 's/-/--/g')
 
 sync
 
-dd if=/dev/$LVM_VG_PATH/$LVM_LV_PATH bs=32M 2>/dev/null | xz -T2 -3 > "$TEMP_FILE" || { echo "Backup failed"; exit 1; }
+dd if="$DISK_DEVICE" bs=32M 2>/dev/null | xz -T2 -3 > "$TEMP_FILE" || { echo "Backup failed"; exit 1; }
 
 echo "Uploading..."
 sftp_cmd=$(mktemp)
