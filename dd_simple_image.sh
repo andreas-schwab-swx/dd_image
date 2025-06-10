ff98ba079e981468bdfeea640ec1d2a4a55bd051
Andreas Schwab <andreas.schwab@softworx.at>
2025-06-10 11:05:56 +0200

    Modified compression algorithm

--- dd_simple_image.sh
+++ dd_simple_image.sh
@@ -11,7 +11,7 @@ source "$CONFIG_FILE"
 
 # Generate backup filename
 CURRENT_DATE=$(date +"%Y-%m-%d-%H-%M")
-BACKUP_FILENAME="image-$CURRENT_DATE.img.xz"  # Generate backup filename
+BACKUP_FILENAME="image-$CURRENT_DATE.img.zst"  # Generate backup filename
 LOGFILE="$LOG_DIR/image-$CURRENT_DATE.log"    # Generate log filename
 
 # Create log directory and remove old logs
@@ -86,10 +86,6 @@ find "$MOUNT_DIR" -name "image-*.img.xz" -mtime +$RETENTION_DAYS -delete 2>/dev/
 # Sync filesystem to ensure data consistency
 sync
 
-# Calculate size of 5% of the disk device
-DEVICE_SIZE=$(blockdev --getsize64 "$DISK_DEVICE")
-TOTAL_BLOCKS=$((DEVICE_SIZE / 33554432))
-BLOCKS_5_PERCENT=$((TOTAL_BLOCKS * 1 / 100))
-
 # Perform backup using dd, mbuffer, and xz
-dd if="$DISK_DEVICE" bs=32M count="$BLOCKS_5_PERCENT" status=progress | mbuffer -m 1G -q | xz -T3 -3 > "$MOUNT_DIR/$BACKUP_FILENAME"
+dd if="$DISK_DEVICE" bs=32M status=progress | mbuffer -m 1G -q | zstd -T3 -3 > "$MOUNT_DIR/$BACKUP_FILENAME"
+#dd if="$DISK_DEVICE" bs=32M status=progress | mbuffer -m 1G -q | xz -T3 -3 > "$MOUNT_DIR/$BACKUP_FILENAME"
